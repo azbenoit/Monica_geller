f4ce496589279c300275ebc4e436d866893ffcf6
flaviagrasso <104382475+flaviagrasso@users.noreply.github.com>
2022-04-29 23:51:56 +0200

    going down

--- SCRIPTS/script-team.js
+++ SCRIPTS/script-team.js
@@ -2,13 +2,20 @@
 
 const roles = document.querySelectorAll(".role");
 const points = document.querySelectorAll(".point");
+const names = []
+const textphoto = document.querySelector(".textphoto");
+console.log("textphoto")
 
 for(let k=0; k<points.length; k++){
     const point = points[k]
+    //append name to the names list
+    names[k] = point.parentNode.id;
     //add the hover function
     point.addEventListener("mouseenter", hover);
-    point.addEventListener("mouseleave", unhover)
+    point.addEventListener("mouseleave", unhover);
 
+    //add the send to the bottom function
+    point.addEventListener("click", sendtorole);
 }
 
 function hover(event){
@@ -16,7 +23,7 @@ function hover(event){
     const relement = pelement.nextElementSibling;
     relement.style.display = "block";
     pelement.classList.add("invisible");
-
+    
 }
 
 function unhover(event){
@@ -24,4 +31,18 @@ function unhover(event){
     const relement = pelement.nextElementSibling;
     relement.style.display = "none";
     pelement.classList.remove("invisible");
+}
+
+function sendtorole(event){
+    console.log("hk");
+    const element = event.currentTarget;
+    const name = element.parentNode.id;
+    const nameimg = document.getElementsByClassName(name);
+    let index = names.indexOf(name, 0);
+    let bottomY = element.parentNode.offsetParent.offsetHeight - element.parentNode.offsetTop - element.offsetHeight;
+    let heighttextphoto = textphoto.offsetHeight;
+    console.log(heighttextphoto);
+    console.log(bottomY);
+    //window.scrollBy(0, bottomY + index*heighttextphoto);
+    window.scroll(0, event.clientY + bottomY + index*heighttextphoto);
 }
